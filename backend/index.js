8f8923c8df096f95aaf9524b7ad09a356b6b6cbf
sahiwl <piyushkrray1@gmail.com>
2025-03-06 17:50:56 +0530

    db added

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,21 @@
-// import express from "express"
-const express = require("express")
+import router from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js"
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js"
 
+dotenv.config()
 const app = express()
 
-const port = 3000
+const PORT = process.env.PORT
+connectDB()
 
-
+app.use("/api/auth", authRoutes)
 app.get('/', (req, res) => {
     res.send("Server is live")
 })
 
-app.listen(port,()=>{
-    console.log(`This server is running on port ${port}`);
+
+app.listen(PORT,()=>{
+    console.log(`This server is running on port: `, PORT);
 })
