a2322275310af996b5a6b7f04540d92c262476cf
sahiwl <piyushkrray1@gmail.com>
2025-04-01 20:25:29 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -22,12 +22,17 @@ app.use(cookieParser());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}))
+
+const corsOptions = {
+  origin: [process.env.ORIGIN, process.env.LOCAL], 
+  methods: ['GET','POST','PUT','DELETE','PATCH'],
+  allowedHeaders: ['Content-Type','Authorization'],
+  credentials: true, 
+};
+
+
+app.use(cors(corsOptions))
+app.options("*", cors(corsOptions));
 
 const PORT = process.env.PORT;
 connectDB();
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_BE_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
